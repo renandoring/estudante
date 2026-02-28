fd66349bff04cf08a1bf40413943bee65e3445e9
renandoring <renandoring@gmail.com>
2026-02-28 20:08:21 -0300

    Projeto calcula notas

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import br.com.alura.estudante.model.Estudante;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Estudante estudanteUm = new Estudante();
+
+        estudanteUm.specificationsStudent();
+        estudanteUm.calculateMedia();
+    }
+}
---
+++ src/br/com/alura/estudante/model/Estudante.java
@@ -0,0 +1,59 @@
+package br.com.alura.estudante.model;
+
+import java.util.Scanner;
+
+public class Estudante {
+    private String name = "XXXXX XXXX";
+    private double note1 = 0;
+    private double note2 = 0;
+    private double note3 = 0;
+
+    public double getNote1() {
+        return note1;
+    }
+
+    public double getNote2() {
+        return note2;
+    }
+
+    public double getNote3() {
+        return note3;
+    }
+
+    public void setNote1(double note1) {
+        this.note1 = note1;
+    }
+
+    public void setNote2(double note2) {
+        this.note2 = note2;
+    }
+
+    public void setNote3(double note3) {
+        this.note3 = note3;
+    }
+
+    //Technique specifications
+    public void specificationsStudent() {
+        System.out.println("The name of student is: " + name +
+                "\nThe students note appears below.");
+    }
+
+    //Get media notes
+    public void calculateMedia() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the notes of students \n");
+        note1 = input.nextInt();
+        note2 = input.nextInt();
+        note3 = input.nextInt();
+
+        double calculate = (note1 + note2 + note3) / 3;
+        System.out.printf("The media is: %.2f\n", calculate);
+
+        //It shows whether the student passed the year.
+        if (calculate >= 7) {
+            System.out.println("CONGRATULATIONS. YOU PASSED THE YEAR!!!");
+        } else {
+            System.out.println("Sorry, you´re failed.");
+        }
+    }
+}
