da68af9660de8aaf9222560afae1e40b1c5f3dd3
renandoring <renandoring@gmail.com>
2026-03-01 14:27:50 -0300

    Alteracao estudo de ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
 import br.com.alura.estudante.model.Estudante;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        Estudante estudanteUm = new Estudante();
+        Estudante estudanteUm = new Estudante("João", 3, 5, 6);
+        Estudante estudanteDois = new Estudante("Maria", 7, 8, 9);
+        Estudante estudanteTres = new Estudante("Pedro", 9, 9, 8);
 
-        estudanteUm.specificationsStudent();
-        estudanteUm.calculateMedia();
+        ArrayList<Estudante> estudantes = new ArrayList<>();
+
+        estudantes.add(estudanteUm);
+        estudantes.add(estudanteDois);
+        estudantes.add(estudanteTres);
+
+        //System.out.println("Tamanho da lista: " + estudantes.size());
+        //System.out.println("Terceiro aluno " + estudantes.get(2));
+
+
+        System.out.println("\nImpressão da lista abaixo: ");
+
+        for (Estudante estudante : estudantes) {
+            System.out.println(estudante + "\n");
+
+            double calculate = (estudante.getNote1() + estudante.getNote2() + estudante.getNote3()) / 3;
+            System.out.printf("The media is: %.2f\n", calculate);
+
+            //It shows whether the student passed the year.
+            if (calculate >= 7) {
+                System.out.println("CONGRATULATIONS. YOU PASSED THE YEAR!!! \n");
+            } else {
+                System.out.println("Sorry, you´re failed. \n");
+            }
+        }
     }
 }
--- src/br/com/alura/estudante/model/Estudante.java
+++ src/br/com/alura/estudante/model/Estudante.java
@@ -32,6 +32,17 @@ public class Estudante {
         this.note3 = note3;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Estudante(String name, int note1, int note2, int note3) {
+        this.name = name;
+        this.note1 = note1;
+        this.note2 = note2;
+        this.note3 = note3;
+    }
+
     //Technique specifications
     public void specificationsStudent() {
         System.out.println("The name of student is: " + name +
@@ -39,21 +50,29 @@ public class Estudante {
     }
 
     //Get media notes
-    public void calculateMedia() {
-        Scanner input = new Scanner(System.in);
+   // public void calculateMedia() {
+        /*Scanner input = new Scanner(System.in);
         System.out.println("Enter the notes of students \n");
         note1 = input.nextInt();
         note2 = input.nextInt();
-        note3 = input.nextInt();
+        note3 = input.nextInt();*/
 
-        double calculate = (note1 + note2 + note3) / 3;
-        System.out.printf("The media is: %.2f\n", calculate);
+        //double calculate = (note1 + note2 + note3) / 3;
+        //System.out.printf("The media is: %.2f\n", calculate);
 
         //It shows whether the student passed the year.
-        if (calculate >= 7) {
-            System.out.println("CONGRATULATIONS. YOU PASSED THE YEAR!!!");
+    /*    if (calculate >= 7) {
+            System.out.println("CONGRATULATIONS. YOU PASSED THE YEAR!!! \n");
         } else {
-            System.out.println("Sorry, you´re failed.");
-        }
+            System.out.println("Sorry, you´re failed. \n");
+        }*/
+    //}
+
+    @Override
+    public String toString() {
+        return "nome: " + this.getName() +
+                "\nNota Um: " + this.getNote1() +
+                "\nNota Dois: " + this.getNote2() +
+                "\nNota Dois:" + this.getNote3();
     }
 }
