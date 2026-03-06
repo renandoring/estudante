cc823d389ea20f01e4ca37e26c9f08610162db36
renandoring <renandoring@gmail.com>
2026-03-06 20:25:06 -0300

    Add JSON

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,31 @@
 import br.com.alura.estudante.model.Estudante;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
+        //CHAMADA JSON E TTO DE UPPERCASE
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        //ARRAY
+        ArrayList<Estudante> estudantes = new ArrayList<>();
+
         Estudante estudanteUm = new Estudante("João", 3, 5, 6);
         Estudante estudanteDois = new Estudante("Maria", 7, 8, 9);
         Estudante estudanteTres = new Estudante("Pedro", 9, 9, 8);
 
-        ArrayList<Estudante> estudantes = new ArrayList<>();
-
         estudantes.add(estudanteUm);
         estudantes.add(estudanteDois);
         estudantes.add(estudanteTres);
-
-        //System.out.println("Tamanho da lista: " + estudantes.size());
-        //System.out.println("Terceiro aluno " + estudantes.get(2));
-
 
         System.out.println("\nImpressão da lista abaixo: ");
 
@@ -25,6 +33,7 @@ public class Main {
             System.out.println(estudante + "\n");
 
             double calculate = (estudante.getNote1() + estudante.getNote2() + estudante.getNote3()) / 3;
+
             System.out.printf("The media is: %.2f\n", calculate);
 
             //It shows whether the student passed the year.
@@ -33,6 +42,13 @@ public class Main {
             } else {
                 System.out.println("Sorry, you´re failed. \n");
             }
+        }
+        try (FileWriter escreva = new FileWriter("estudantes.json")) {
+            escreva.write(gson.toJson(estudantes));
+            escreva.close();
+            System.out.println("Arquivo criado com SUCESSO!");
+        } catch (IOException ex) {
+            System.out.println("Erro ao criar o arquivo!" + ex.getMessage());
         }
     }
 }
--- src/br/com/alura/estudante/model/Estudante.java
+++ src/br/com/alura/estudante/model/Estudante.java
@@ -1,5 +1,7 @@
 package br.com.alura.estudante.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Estudante {
@@ -7,6 +9,20 @@ public class Estudante {
     private double note1 = 0;
     private double note2 = 0;
     private double note3 = 0;
+    private double media;//VAI ARMAZENAR A MEDIA
+
+    //CONSTRUTOR
+    public Estudante(String name, int note1, int note2, int note3) {
+        this.name = name;
+        this.note1 = note1;
+        this.note2 = note2;
+        this.note3 = note3;
+        this.media = (note1 + note2 + note3) / 3.0;
+    }
+
+    public Double getMedia() {
+        return (note1 + note2 + note3) / 3;
+    }
 
     public double getNote1() {
         return note1;
@@ -36,31 +52,25 @@ public class Estudante {
         return name;
     }
 
-    public Estudante(String name, int note1, int note2, int note3) {
-        this.name = name;
-        this.note1 = note1;
-        this.note2 = note2;
-        this.note3 = note3;
-    }
-
     //Technique specifications
     public void specificationsStudent() {
         System.out.println("The name of student is: " + name +
                 "\nThe students note appears below.");
     }
 
+
     //Get media notes
-   // public void calculateMedia() {
+    // public void calculateMedia() {
         /*Scanner input = new Scanner(System.in);
         System.out.println("Enter the notes of students \n");
         note1 = input.nextInt();
         note2 = input.nextInt();
         note3 = input.nextInt();*/
 
-        //double calculate = (note1 + note2 + note3) / 3;
-        //System.out.printf("The media is: %.2f\n", calculate);
+    //double calculate = (note1 + note2 + note3) / 3;
+    //System.out.printf("The media is: %.2f\n", calculate);
 
-        //It shows whether the student passed the year.
+    //It shows whether the student passed the year.
     /*    if (calculate >= 7) {
             System.out.println("CONGRATULATIONS. YOU PASSED THE YEAR!!! \n");
         } else {
@@ -73,6 +83,7 @@ public class Estudante {
         return "nome: " + this.getName() +
                 "\nNota Um: " + this.getNote1() +
                 "\nNota Dois: " + this.getNote2() +
-                "\nNota Dois:" + this.getNote3();
+                "\nNota Dois:" + this.getNote3() +
+                "\nMedia: " + this.getMedia();
     }
 }
